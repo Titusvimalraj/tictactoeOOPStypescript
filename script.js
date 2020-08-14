dd4ea1ed6983bc6f852c7ec5c77d1188c4aafc7b
Titus Vimal Raj <titusvimalraj@gmail.com>
2020-08-14 12:41:41 +0530

    game inital running!

--- script.js
+++ script.js
@@ -11,14 +11,101 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var board = new Array(3).fill('').map(function () { return new Array(3).fill(''); });
+var board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
 var gameOn = true;
+var player1;
+var player1Marker;
+var player2;
+var player2Marker;
+var playerMarker = "O";
+function updateMarkerListener(ele) {
+    // console.log("ele playerMarker");
+    // console.log(ele, playerMarker);
+    document.getElementById(ele).innerHTML = playerMarker;
+    board[ele[0]][ele[1]] = playerMarker;
+    var old_element = document.getElementById("board");
+    var new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+    var game = new Game();
+    if (game.checkDraw(board)) {
+        console.log("Draw!");
+        document.getElementById("tic-player-turn").innerHTML = "Draw!";
+        return;
+    }
+    if (game.checkWin(board, playerMarker)) {
+        console.log((playerMarker == "O" ? player1 : player2) + " wins!");
+        document.getElementById("tic-player-turn").innerHTML = (playerMarker == "O" ? player1 : player2) + " wins!";
+        return;
+    }
+    var nextMark = playerMarker == "O" ? "X" : "O";
+    // console.log("nextMark", nextMark);
+    playerMarker = nextMark;
+    var playerTurnMessage = (nextMark === "O" ? player1 : player2) + " your turn!";
+    // console.log("Player Marker", nextMark);
+    // console.log(`${nextMark === "O" ? player1 : player2} your turn!`);
+    document.getElementById("tic-player-turn").innerHTML = playerTurnMessage;
+    var newGameBoard = new Board(board);
+    // console.log(board);
+    newGameBoard.addEventListnerToBoard(board, nextMark);
+    return;
+}
+var startGame = function () {
+    player1 = prompt("Player One: Enter Your Name , you will be O");
+    player1Marker = "O";
+    player2 = prompt("Player Two: Enter Your Name, you will be X");
+    player2Marker = "X";
+    var playerTurnMessage = (playerMarker === "O" ? player1 : player2) + " your turn!";
+    console.log("Player Marker", playerMarker);
+    console.log((playerMarker === "O" ? player1 : player2) + " your turn!");
+    document.getElementById("tic-player-turn").innerHTML = playerTurnMessage;
+    var newGameBoard = new Board(board);
+    newGameBoard.addEventListnerToBoard(board, playerMarker);
+    newGameBoard.printBoard(board);
+};
 var Board = /** @class */ (function () {
     function Board(board) {
         this.board = board;
     }
     Board.prototype.printBoard = function (boardValue) {
-        console.log(boardValue);
+        this.board = boardValue;
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                document.getElementById("" + i + j).innerHTML = this.board[i][j];
+            }
+        }
+    };
+    Board.prototype.clearBoard = function () {
+        return board;
+    };
+    Board.prototype.addEventListnerToBoard = function (board, player) {
+        var _loop_1 = function (i) {
+            var _loop_2 = function (j) {
+                // console.log("the filled value", board[i][j]);
+                if (board[i][j] == "") {
+                    // console.log("empty element");
+                    // console.log(`${i}${j}`);
+                    // console.log("player marker during event listener", player);
+                    document
+                        .getElementById("" + i + j)
+                        .addEventListener("click", function () {
+                        updateMarkerListener("" + i + j);
+                    });
+                }
+            };
+            for (var j = 0; j < 3; j++) {
+                _loop_2(j);
+            }
+        };
+        // console.log("adding Event listener");
+        for (var i = 0; i < 3; i++) {
+            _loop_1(i);
+        }
+        console.log(board);
+        return board;
     };
     return Board;
 }());
@@ -32,7 +119,7 @@ var Player = /** @class */ (function (_super) {
     Player.prototype.move = function (board, position) {
         this.marker;
         var x = position.x, y = position.y;
-        if (board[x][y] == '') {
+        if (board[x][y] == "") {
             board[x][y] == this.marker;
             return board;
         }
@@ -47,24 +134,55 @@ var Game = /** @class */ (function () {
     function Game() {
     }
     Game.prototype.checkWin = function (board, playerMarker) {
-        if (board[0][0] === playerMarker && board[0][1] === playerMarker && board[0][2] === playerMarker ||
-            board[1][0] === playerMarker && board[1][1] === playerMarker && board[1][2] === playerMarker ||
-            board[2][0] === playerMarker && board[2][1] === playerMarker && board[2][2] === playerMarker ||
-            board[0][0] === playerMarker && board[1][1] === playerMarker && board[2][2] === playerMarker ||
-            board[0][0] === playerMarker && board[1][1] === playerMarker && board[2][2] === playerMarker ||
-            board[0][2] === playerMarker && board[1][1] === playerMarker && board[2][0] === playerMarker ||
-            board[0][0] === playerMarker && board[1][0] === playerMarker && board[2][0] === playerMarker ||
-            board[0][1] === playerMarker && board[1][1] === playerMarker && board[2][1] === playerMarker ||
-            board[0][2] === playerMarker && board[1][2] === playerMarker && board[2][2] === playerMarker) {
+        if ((board[0][0] === playerMarker &&
+            board[0][1] === playerMarker &&
+            board[0][2] === playerMarker) ||
+            (board[1][0] === playerMarker &&
+                board[1][1] === playerMarker &&
+                board[1][2] === playerMarker) ||
+            (board[2][0] === playerMarker &&
+                board[2][1] === playerMarker &&
+                board[2][2] === playerMarker) ||
+            (board[0][0] === playerMarker &&
+                board[1][1] === playerMarker &&
+                board[2][2] === playerMarker) ||
+            (board[0][0] === playerMarker &&
+                board[1][1] === playerMarker &&
+                board[2][2] === playerMarker) ||
+            (board[0][2] === playerMarker &&
+                board[1][1] === playerMarker &&
+                board[2][0] === playerMarker) ||
+            (board[0][0] === playerMarker &&
+                board[1][0] === playerMarker &&
+                board[2][0] === playerMarker) ||
+            (board[0][1] === playerMarker &&
+                board[1][1] === playerMarker &&
+                board[2][1] === playerMarker) ||
+            (board[0][2] === playerMarker &&
+                board[1][2] === playerMarker &&
+                board[2][2] === playerMarker)) {
             return true;
         }
         else {
             return false;
         }
     };
+    Game.prototype.checkDraw = function (board) {
+        for (var i = 0; i < board.length; i++) {
+            for (var j = 0; j < 3; j++) {
+                if (!(board[i][j] == "")) {
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+    Game.prototype.resetGame = function () {
+        location.reload();
+    };
     return Game;
 }());
-var newGameBoard = new Board(board);
-console.log(newGameBoard);
-// while(gameOn){
-// }
+startGame();
--- script.ts
+++ script.ts
@@ -1,83 +1,200 @@
-type markerType = 'X' | 'O' | '';
+type markerType = "X" | "O" | "";
 
 type boardType = markerType[][];
-type positionCoordinates = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 ;
+type positionCoordinates = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 interface coordinates {
-    'x':positionCoordinates,
-    'y':positionCoordinates;
+  x: number;
+  y: number;
 }
 
-const board:markerType[][] = new Array(3).fill('').map(() => new Array(3).fill(''));
-let gameOn:boolean=true;
-class Board{    
-    board:boardType;
-    constructor(board:boardType){
-        this.board = board;
-    }
+const board: markerType[][] = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
-    printBoard(boardValue:Board){
-        console.log(boardValue);
-    }
-}
+let gameOn: boolean = true;
 
-class Player extends Board{
-    marker:markerType;
-
-    constructor(marker:markerType, board:boardType){
-        super(board);
-        this.marker = marker;
-    }
-
-    move(board:Board, position:coordinates):Board{    
-        this.marker;
-        let {x,y} = position;
-        if(board[x][y]==''){
-            board[x][y] == this.marker;
-            return board;
-        }else{
-            this.move(board,position);
-            return board;
-        }      
+let player1: string;
+let player1Marker: string;
+let player2: string;
+let player2Marker: string;
+let playerMarker: markerType = "O";
+function updateMarkerListener(ele: string): string {
+  // console.log("ele playerMarker");
+  // console.log(ele, playerMarker);
+  document.getElementById(ele).innerHTML = playerMarker;
+  board[ele[0]][ele[1]] = playerMarker;  
+  let old_element = document.getElementById("board");
+  let new_element = old_element.cloneNode(true);
+  old_element.parentNode.replaceChild(new_element, old_element);
+  let game = new Game();
+  if (game.checkDraw(board)) {
+    console.log(`Draw!`);
+    document.getElementById("tic-player-turn").innerHTML = "Draw!";
+    return;
+  }
+  if (game.checkWin(board, playerMarker)) {
     
-    }
+    console.log(`${playerMarker == "O" ? player1 : player2} wins!`);
+    document.getElementById("tic-player-turn").innerHTML = `${playerMarker == "O" ? player1 : player2} wins!`;
+    return;
+  }
+  let nextMark: markerType = playerMarker == "O" ? "X" : "O";
+  // console.log("nextMark", nextMark);
+  playerMarker = nextMark;
+
+  let playerTurnMessage: string = `${
+    nextMark === "O" ? player1 : player2
+  } your turn!`;
+  // console.log("Player Marker", nextMark);
+  // console.log(`${nextMark === "O" ? player1 : player2} your turn!`);
+  document.getElementById("tic-player-turn").innerHTML = playerTurnMessage;
+
+  let newGameBoard = new Board(board);
+  // console.log(board);
+  newGameBoard.addEventListnerToBoard(board, nextMark);
+
+  return;
 }
 
-class Game{
-    
-    constructor(){
+let startGame = () => {
+  player1 = prompt("Player One: Enter Your Name , you will be O");
+  player1Marker = "O";
 
+  player2 = prompt("Player Two: Enter Your Name, you will be X");
+  player2Marker = "X";
+
+  let playerTurnMessage: string = `${
+    playerMarker === "O" ? player1 : player2
+  } your turn!`;
+  console.log("Player Marker", playerMarker);
+  console.log(`${playerMarker === "O" ? player1 : player2} your turn!`);
+  document.getElementById("tic-player-turn").innerHTML = playerTurnMessage;
+
+  let newGameBoard = new Board(board);
+  newGameBoard.addEventListnerToBoard(board, playerMarker);
+  newGameBoard.printBoard(board);
+};
+
+class Board {
+  board: boardType;
+  constructor(board: boardType) {
+    this.board = board;
+  }
+
+  printBoard(boardValue: boardType) {
+    this.board = boardValue;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        document.getElementById(`${i}${j}`).innerHTML = this.board[i][j];
+      }
     }
+  }
 
-    checkWin(board:boardType, playerMarker:markerType):boolean{
-        if(
-            board[0][0]===playerMarker && board[0][1]===playerMarker && board[0][2]===playerMarker ||
-            board[1][0]===playerMarker && board[1][1]===playerMarker && board[1][2]===playerMarker ||
-            board[2][0]===playerMarker && board[2][1]===playerMarker && board[2][2]===playerMarker ||
-            board[0][0]===playerMarker && board[1][1]===playerMarker && board[2][2]===playerMarker ||
-            board[0][0]===playerMarker && board[1][1]===playerMarker && board[2][2]===playerMarker ||
-            board[0][2]===playerMarker && board[1][1]===playerMarker && board[2][0]===playerMarker ||
-            board[0][0]===playerMarker && board[1][0]===playerMarker && board[2][0]===playerMarker ||
-            board[0][1]===playerMarker && board[1][1]===playerMarker && board[2][1]===playerMarker ||
-            board[0][2]===playerMarker && board[1][2]===playerMarker && board[2][2]===playerMarker
-        ){
+  clearBoard(): boardType {
+    return board;
+  }
 
-            return true;
-
-        }else{
-
-            return false;
-
+  addEventListnerToBoard(board: boardType, player: markerType): boardType {
+    // console.log("adding Event listener");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // console.log("the filled value", board[i][j]);
+        if (board[i][j] == "") {
+          // console.log("empty element");
+          // console.log(`${i}${j}`);
+          // console.log("player marker during event listener", player);
+          document
+            .getElementById(`${i}${j}`)
+            .addEventListener("click", function () {
+              updateMarkerListener(`${i}${j}`);
+            });
         }
-
-
+      }
     }
+    console.log(board);
+    return board;
+  }
 }
 
-let newGameBoard = new Board(board);
+class Player extends Board {
+  marker: markerType;
 
-console.log(newGameBoard);
-// while(gameOn){
+  constructor(marker: markerType, board: boardType) {
+    super(board);
+    this.marker = marker;
+  }
 
+  move(board: boardType, position: coordinates): boardType {
+    this.marker;
+    let { x, y } = position;
+    if (board[x][y] == "") {
+      board[x][y] == this.marker;
+      return board;
+    } else {
+      this.move(board, position);
+      return board;
+    }
+  }
+}
 
-// }
+class Game {
+  constructor() {}
+
+  checkWin(board: boardType, playerMarker: markerType): boolean {
+    if (
+      (board[0][0] === playerMarker &&
+        board[0][1] === playerMarker &&
+        board[0][2] === playerMarker) ||
+      (board[1][0] === playerMarker &&
+        board[1][1] === playerMarker &&
+        board[1][2] === playerMarker) ||
+      (board[2][0] === playerMarker &&
+        board[2][1] === playerMarker &&
+        board[2][2] === playerMarker) ||
+      (board[0][0] === playerMarker &&
+        board[1][1] === playerMarker &&
+        board[2][2] === playerMarker) ||
+      (board[0][0] === playerMarker &&
+        board[1][1] === playerMarker &&
+        board[2][2] === playerMarker) ||
+      (board[0][2] === playerMarker &&
+        board[1][1] === playerMarker &&
+        board[2][0] === playerMarker) ||
+      (board[0][0] === playerMarker &&
+        board[1][0] === playerMarker &&
+        board[2][0] === playerMarker) ||
+      (board[0][1] === playerMarker &&
+        board[1][1] === playerMarker &&
+        board[2][1] === playerMarker) ||
+      (board[0][2] === playerMarker &&
+        board[1][2] === playerMarker &&
+        board[2][2] === playerMarker)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  checkDraw(board: boardType): boolean {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (!(board[i][j] == "")) {
+          continue;
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+  resetGame(): void {
+    location.reload();
+  }
+}
+
+startGame();
